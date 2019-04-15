e48a4dd1a1c60e77e95dd8574f4ad0ffb56648cf
baboulinet <vergilotaku@live.fr>
2019-04-15 21:34:31 +0200

    added PrintTask function

--- todo.go
+++ todo.go
@@ -8,12 +8,19 @@ const (
 	RedColor = "\033[1;31m%s\033[0m"
 )
 
+// Task : Data structure containing all the informations from a Task
 type Task struct {
 	id          uint16
 	description string
 	status      bool
 }
 
+// PrintTask : Print to the right format the task passed in parameter
+func PrintTask(pTask Task) {
+	fmt.Printf(RedColor, fmt.Sprint(pTask.id)+". "+pTask.description)
+}
+
 func main() {
-	fmt.Printf(RedColor, "☑ Test")
+	t := Task{id: 1, description: "Je suis une tache.", status: false}
+	PrintTask(t)
 }
